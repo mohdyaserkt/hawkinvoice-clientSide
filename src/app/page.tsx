583ac3d7a933bdcf14f670ea08bf84f79ee8d586
mohdyaserkt <yaserkt786@gmail.com>
2023-07-31 06:59:12 +0000

    31 Jul 2023, 12:29

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,6 +3,13 @@ import Image from 'next/image'
 export default function Home() {
   return (
    <>
+   <div>
+    <nav></nav>
+    <div>
+      <div></div>
+      <div></div>
+    </div>
+   </div>
    </>
   )
 }
