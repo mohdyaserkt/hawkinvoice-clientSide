237511a4c53532b414e7faca689f2993d3ef0061
mohdyaserkt <yaserkt786@gmail.com>
2023-07-31 07:21:57 +0000

    31 Jul 2023, 12:51

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,10 +3,11 @@ import Image from 'next/image'
 export default function Home() {
   return (
    <>
-   <div className='w-screen h-screen bg-black'>
+   <div className='w-screen h-screen '>
     <nav></nav>
-    <div className='bg-white w-631px h-[728px]'>
-      
+    <div className='flex h-full justify-center items-center'>
+    <div className='bg-white w-9/12 h-10'>
+      </div>
     </div>
    </div>
    </>
