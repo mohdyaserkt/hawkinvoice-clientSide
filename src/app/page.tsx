aa5ee1c483ec2b5b15906c94e8b1bda5af72edd1
mohdyaserkt <yaserkt786@gmail.com>
2023-07-31 09:51:46 +0000

    31 Jul 2023, 15:21

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,12 +6,12 @@ export default function Home() {
       <div className='w-screen h-screen '>
         <nav></nav>
         <div className='flex h-full justify-center items-center'>
-          <div className='bg-white w-1/3 h-4/5 rounded-s-[80px]  rounded-t-[80px]'>
+          <div className='bg-white w-1/3 h-4/5 rounded-s-[80px]  rounded-t-[80px]  flex flex-col   items-center '>
             <div>
-              <div>Signup up for Figma</div>
+              <div className='text-2xl'>Signup up for Figma</div>
               <button>
                 <div>
-
+                  continue with Google
                 </div>
               </button>
               <span>or</span>
@@ -19,17 +19,27 @@ export default function Home() {
             <div></div>
             <input type="text" />
             <input type="text" />
+            <div></div>
+            <div>
+              <input type="text" />
+              <label htmlFor=""></label>
+            </div>
+            <div></div>
+            <button>Create account</button>
+            <div>
+              <span>By clicking "Create account" or "Continue with Google", you agree to the </span>
+            </div>
+
+            <div>
+              <div>
+                <a href="">Use sigle sign-on</a>
+              </div>
+              <div>
+                already have an account?<a href="">Log in</a>
+              </div>
+            </div>
           </div>
-          <div></div>
-          <div>
-            <input type="text" />
-            <label htmlFor=""></label>
-          </div>
-          <div></div>
-          <button>Create account</button>
-          <div>
-            <span></span>
-          </div>
+
         </div>
       </div>
     </>
