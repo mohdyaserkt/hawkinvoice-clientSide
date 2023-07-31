92ebb8cf1a99f4f94e872c809d1c17ebf3895257
mohdyaserkt <yaserkt786@gmail.com>
2023-07-31 07:40:41 +0000

    31 Jul 2023, 13:10

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,7 +6,7 @@ export default function Home() {
       <div className='w-screen h-screen '>
         <nav></nav>
         <div className='flex h-full justify-center items-center'>
-          <div className='bg-white w-1/3 h-4/6 rounded-s-[80px]  rounded-t-[80px]'>
+          <div className='bg-white w-1/3 h-3/4 rounded-s-[80px]  rounded-t-[80px]'>
           </div>
         </div>
       </div>
