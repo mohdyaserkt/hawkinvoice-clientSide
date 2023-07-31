53367a12e5f75947ff3acc3ae81acc2490b1af55
mohdyaserkt <yaserkt786@gmail.com>
2023-07-31 09:20:00 +0000

    31 Jul 2023, 14:50

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,6 +7,10 @@ export default function Home() {
         <nav></nav>
         <div className='flex h-full justify-center items-center'>
           <div className='bg-white w-1/3 h-4/5 rounded-s-[80px]  rounded-t-[80px]'>
+            <div>
+              <div>Signup up for Figma</div>
+              <button></button>
+            </div>
           </div>
         </div>
       </div>
