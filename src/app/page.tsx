319cca7fcc7d1b85956e7d09a0dbf6a693963e01
mohdyaserkt <yaserkt786@gmail.com>
2023-07-31 10:25:12 +0000

    31 Jul 2023, 15:55

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,9 +7,9 @@ export default function Home() {
         <nav></nav>
         <div className='flex h-full justify-center items-center'>
           <div className='bg-white w-1/3 h-4/5 rounded-s-[80px]  rounded-t-[80px]  flex flex-col   items-center justify-center '>
-            <div>
-              <div className='text-3xl font-semibold'>Signup up for Figma</div>
-              <button className='mt-8 border'>
+            <div className='flex flex-col  items-center justify-center'>
+              <div className='text-2xl font-semibold'>Signup up for Figma</div><br />
+              <button className='mt-8 border-2 border-black rounded'>
                 <div>
                   continue with Google
                 </div>
