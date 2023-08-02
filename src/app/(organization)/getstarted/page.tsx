97208fa48e45a25d61445b0715fc16f2e0e1bb5c
mohdyaserkt <yaserkt786@gmail.com>
2023-08-02 09:17:09 +0000

    2 Aug 2023, 14:47

--- src/app/(organization)/getstarted/page.tsx
+++ src/app/(organization)/getstarted/page.tsx
@@ -7,15 +7,16 @@ const getStarted = () => {
       <nav></nav>
       <div className='flex h-full justify-center items-center'>
         <div className='bg-red-600 w-2/5 h-[200px]'>
-          <div>Let’s get started</div>
-          <div>
-            <h1>Tell us about your business</h1>
+          <div className='text-white text-base'>Let’s get started</div>
+          <div className='mt-2 mb-4'>
+            <h1 className='text-white text-3xl'>Tell us about your business</h1>
           </div>
-          <div>
-            <p>
+          <div className='mb-9'>
+            <p className='text-white text-base'>
             Every business is unique. We want to hear about yours. If you registered your business with IRS, make sure the information you submit matches what is on your IRS documents.
             </p>
           </div>
+          
           
 
         </div>
