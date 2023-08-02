5ae1898f066aa022f6c00451f4d8031d9e9d4510
mohdyaserkt <yaserkt786@gmail.com>
2023-08-02 09:58:13 +0000

    2 Aug 2023, 15:28

--- src/app/(organization)/getstarted/page.tsx
+++ src/app/(organization)/getstarted/page.tsx
@@ -1,30 +1,48 @@
+
 import React from 'react'
 
 
 const getStarted = () => {
   return (
-    <> 
-    <div className='h-screen w-screen '>
-      <nav></nav>
-      <div className='flex h-full justify-center items-center'>
-        <div className='bg-red-600 w-2/5 h-[200px]'>
-          <div className='text-white text-base'>Let’s get started</div>
-          <div className='mt-2 mb-4'>
-            <h1 className='text-white text-3xl'>Tell us about your business</h1>
+    <>
+      <div className='h-screen w-screen '>
+        <nav></nav>
+        <div className='flex h-full justify-center items-center'>
+          <div className='bg-red-600 w-2/5 h-2/4'>
+            <div className='text-white text-base'>Let’s get started</div>
+            <div className='mt-2 mb-4'>
+              <h1 className='text-white text-3xl'>Tell us about your business</h1>
+            </div>
+            <div className='mb-9'>
+              <p className='text-white text-base'>
+                Every business is unique. We want to hear about yours. If you registered your business with IRS, make sure the information you submit matches what is on your IRS documents.
+              </p>
+            </div>
+
+            <div className="relative h-10 w-72  min-w-[200px]">
+              <select className="peer h-full w-full rounded-[7px] border border-blue-gray-200 border-t-transparent bg-transparent px-3 py-2.5 font-sans text-sm font-normal text-blue-gray-700 outline outline-0 transition-all placeholder-shown:border placeholder-shown:border-blue-gray-200 placeholder-shown:border-t-blue-gray-200 empty:!bg-red-500 focus:border-2 focus:border-white focus:border-t-transparent focus:outline-0 disabled:border-0 disabled:bg-blue-gray-50">
+                <option value="brazil">Brazil</option>
+                <option value="bucharest">Bucharest</option>
+                <option value="london">London</option>
+                <option value="washington">Washington</option>
+              </select>
+              <label className="before:content[' '] after:content[' '] pointer-events-none absolute left-0 -top-1.5 flex h-full w-full select-none text-[11px] font-normal leading-tight text-green-600 transition-all before:pointer-events-none before:mt-[6.5px] before:mr-1 before:box-border before:block before:h-1.5 before:w-2.5 before:rounded-tl-md before:border-t before:border-l before:border-blue-gray-200 before:transition-all after:pointer-events-none after:mt-[6.5px] after:ml-1 after:box-border after:block after:h-1.5 after:w-2.5 after:flex-grow after:rounded-tr-md after:border-t after:border-r after:border-blue-gray-200 after:transition-all peer-placeholder-shown:text-sm peer-placeholder-shown:leading-[3.75] peer-placeholder-shown:text-blue-gray-500 peer-placeholder-shown:before:border-transparent peer-placeholder-shown:after:border-transparent peer-focus:text-[11px] peer-focus:leading-tight peer-focus:text-green-600 peer-focus:before:border-t-2 peer-focus:before:border-l-2 peer-focus:before:border-white peer-focus:after:border-t-2 peer-focus:after:border-r-2 peer-focus:after:border-white peer-disabled:text-transparent peer-disabled:before:border-transparent peer-disabled:after:border-transparent peer-disabled:peer-placeholder-shown:text-blue-gray-500">
+                Select a City
+              </label>
+            </div>
+
+            
+
+
+
+
           </div>
-          <div className='mb-9'>
-            <p className='text-white text-base'>
-            Every business is unique. We want to hear about yours. If you registered your business with IRS, make sure the information you submit matches what is on your IRS documents.
-            </p>
-          </div>
-          
-          
 
         </div>
 
       </div>
-      
-    </div>
+
+
     </>
 
   )
