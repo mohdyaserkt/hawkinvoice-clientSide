c9b43e96a2d83d08600a73b73427b03fe05af27c
mohdyaserkt <yaserkt786@gmail.com>
2023-08-02 13:16:01 +0000

    2 Aug 2023, 18:46

--- src/app/(organization)/getstarted/page.tsx
+++ src/app/(organization)/getstarted/page.tsx
@@ -22,23 +22,25 @@ const getStarted = () => {
             </div>
 
             <div className="relative h-16 w-full min-w-[200px]">
-  <select className="peer h-full w-full rounded border px-3 py-2.5 text-sm font-normal text-blue-gray-700 outline outline-0 transition-all border-black bg-primary text-white" style={{ borderRadius: '7px' }}>
-    <option className="" value="" hidden selected>
-      What kind of business are you?
-    </option>
-    <option value="brazil" className="rounded bg-white text-black">Brazil</option>
-    <option value="bucharest" className="rounded bg-white text-black">Bucharest</option>
-    <option value="london" className="rounded bg-white text-black">London</option>
-    <option value="washington" className="rounded bg-white text-black">Washington</option>
-  </select>
-</div>
-
+              <select className="peer h-full w-full rounded border px-3 py-2.5 text-sm font-normal text-blue-gray-700 outline outline-0 transition-all border-black bg-primary text-white" style={{ borderRadius: '7px' }}>
+                <option className="" value="" hidden selected>
+                  What kind of business are you?
+                </option>
+                <option value="brazil" className="rounded bg-white text-black">Brazil</option>
+                <option value="bucharest" className="rounded bg-white text-black">Bucharest</option>
+                <option value="london" className="rounded bg-white text-black">London</option>
+                <option value="washington" className="rounded bg-white text-black">Washington</option>
+              </select>
+            </div>
 
             
+
+
+
           </div>
         </div>
       </div>
-      
+
 
     </>
   );
