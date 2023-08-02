195a541c6e3ac1420e2595e1c601d375016cdda1
mohdyaserkt <yaserkt786@gmail.com>
2023-08-02 11:29:17 +0000

    2 Aug 2023, 16:59

--- src/app/(organization)/getstarted/page.tsx
+++ src/app/(organization)/getstarted/page.tsx
@@ -22,11 +22,11 @@ const getStarted = () => {
             </div>
 
             <div className="relative h-16 w-full  min-w-[200px]">
-              <select className="peer h-full w-full rounded border  px-3 py-2.5  text-sm font-normal text-blue-gray-700 outline outline-0 transition-all border-black bg-secondary">
-                <option className="" value="" disabled selected>
+              <select className="peer h-full w-full rounded border  px-3 py-2.5 text-sm font-normal text-blue-gray-700 outline outline-0 transition-all border-black bg-secondary">
+                <option className="" value=""  hidden selected>
                 What kind of business are you?
                 </option>
-                <option value="brazil">Brazil</option>
+                <option  value="brazil">Brazil</option>
                 <option value="bucharest">Bucharest</option>
                 <option value="london">London</option>
                 <option value="washington">Washington</option>
