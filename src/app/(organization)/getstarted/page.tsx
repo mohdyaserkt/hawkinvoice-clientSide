0267f49d4757f1c854303f708f353a2c1be8d583
mohdyaserkt <yaserkt786@gmail.com>
2023-08-03 07:32:09 +0000

    3 Aug 2023, 13:02

--- src/app/(organization)/getstarted/page.tsx
+++ src/app/(organization)/getstarted/page.tsx
@@ -56,7 +56,7 @@ const getStarted = () => {
 
               <input
                 type="text"
-                className="w-full h-16 bg-white border border-gray-300 border-opacity-25 rounded-md px-3 py-2.5 font-sans text-sm text-blue-gray-700 outline-none focus:border-blue-500"
+                className="w-full h-16 bg-white border border-gray-300 border-opacity-25 rounded-md px-3 py-2.5 font-sans text-sm text-blue-gray-700 outline-none  "
               />
             </div>
           </div>
