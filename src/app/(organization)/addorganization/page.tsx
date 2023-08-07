1a1ac00e008ef72b0e959560c16a9c42e26e3966
mohdyaserkt <yaserkt786@gmail.com>
2023-08-07 10:24:17 +0000

    7 Aug 2023, 15:54

---
+++ src/app/(organization)/addorganization/page.tsx
@@ -0,0 +1,19 @@
+import React from "react";
+
+const getStarted = () => {
+  return (
+    <>
+      <div className="h-screen">
+        <nav></nav>
+        <div className="flex h-full justify-center items-center ">
+          <div className="bg-blue-400 w-3/6 h-3/5">
+
+          </div>
+          
+        </div>
+      </div>
+    </>
+  );
+}
+
+export default getStarted;
