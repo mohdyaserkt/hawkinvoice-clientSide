e379514b0cde255669c782c0e70d384fe8736f6e
mohdyaserkt <yaserkt786@gmail.com>
2023-08-14 11:00:02 +0000

    14 Aug 2023, 16:30

--- src/app/(auth)/signup/page.tsx
+++ src/app/(auth)/signup/page.tsx
@@ -1,32 +1,36 @@
-"use client"
-import Image from 'next/image'
-import { FcGoogle } from 'react-icons/fc';
+"use client";
+import Image from "next/image";
+import { FcGoogle } from "react-icons/fc";
 import { useRouter } from "next/navigation";
-import HandleForm from '@/utils/handleFormState';
-interface ISignup{
-  email:string,
-  password:string
+import HandleForm from "@/utils/handleFormState";
+interface ISignup {
+  email: string;
+  password: string;
 }
-    
+
 export default function signup() {
-    const router = useRouter();
-    const [signupState, setSignupState] = HandleForm({ email: "", password: ""});
+  const router = useRouter();
+  const [signupState, setSignupState] = HandleForm({ email: "", password: "" });
+  console.log(signupState);
+  
   return (
     <>
-      <div className='w-screen h-screen '>
+      <div className="w-screen h-screen ">
         <nav></nav>
-        <div className='flex h-full justify-center items-center '>
-          <div className='bg-white md:w-2/3 lg:w-1/3 lg:h-4/5 sm:w-full rounded sm:p-10  md:rounded-s-[5rem]  md:rounded-t-[5rem]  flex flex-col   items-center justify-center '>
-            <div className='flex flex-col  items-center justify-center'>
-              <div className='text-2xl font-ligthbold'>Signup up for hawkinvoice</div><br />
-              <button className='mt-8 border-2 border-black rounded-lg w-[22.375rem] h-[3rem] flex justify-center items-center'>
-              
-                <div className='text-base inline-flex items-center gap-3'>
+        <div className="flex h-full justify-center items-center ">
+          <div className="bg-white md:w-2/3 lg:w-1/3 lg:h-4/5 sm:w-full rounded sm:p-10  md:rounded-s-[5rem]  md:rounded-t-[5rem]  flex flex-col   items-center justify-center ">
+            <div className="flex flex-col  items-center justify-center">
+              <div className="text-2xl font-ligthbold">
+                Signup up for hawkinvoice
+              </div>
+              <br />
+              <button className="mt-8 border-2 border-black rounded-lg w-[22.375rem] h-[3rem] flex justify-center items-center">
+                <div className="text-base inline-flex items-center gap-3">
                   <FcGoogle size={25} />
                   continue with Google
                 </div>
               </button>
-              <span className='text-gray-600 my-5'>or</span>
+              <span className="text-gray-600 my-5">or</span>
             </div>
             <div></div>
 
@@ -34,6 +38,10 @@ export default function signup() {
               <input
                 className="rounded-lg w-[22.375rem] h-[3rem]  peer   border  border-black border-t-transparent bg-transparent px-3 py-2.5  text-sm  text-black outline outline-1 transition-all placeholder-shown:border placeholder-shown:border-black placeholder-shown:border-t-black focus:border-2 focus:border-black focus:border-t-transparent focus:outline-0 disabled:border-0 disabled:bg-blue-gray-50"
                 placeholder=" "
+                name="email"
+                id="email"
+                value={signupState.email}
+                onChange={setSignupState}
               />
               <label className="before:content[' '] after:content[' '] pointer-events-none absolute left-0 -top-1.5 flex h-full w-full select-none text-[11px] font-normal leading-tight text-gray-400 transition-all before:pointer-events-none before:mt-[6.5px] before:mr-1 before:box-border before:block before:h-1.5 before:w-2.5 before:rounded-tl-md before:border-t before:border-l before:border-black before:transition-all after:pointer-events-none after:mt-[6.5px] after:ml-1 after:box-border after:block after:h-1.5 after:w-2.5 after:flex-grow after:rounded-tr-md after:border-t after:border-r after:border-black after:transition-all peer-placeholder-shown:text-sm peer-placeholder-shown:leading-[3.75] peer-placeholder-shown:text-gray-600 peer-placeholder-shown:before:border-transparent peer-placeholder-shown:after:border-transparent peer-focus:text-[11px] peer-focus:leading-tight peer-focus:text-gray-600 peer-focus:before:border-t-2 peer-focus:before:border-l-2 peer-focus:before:border-black peer-focus:after:border-t-2 peer-focus:after:border-r-2 peer-focus:after:border-black peer-disabled:text-transparent peer-disabled:before:border-transparent peer-disabled:after:border-transparent peer-disabled:peer-placeholder-shown:text-gray-600">
                 Email
@@ -43,6 +51,11 @@ export default function signup() {
               <input
                 className="  rounded-lg w-[22.375rem] h-[3rem]  peer   border  border-black border-t-transparent bg-transparent px-3 py-2.5  text-sm  text-black outline outline-1 transition-all placeholder-shown:border placeholder-shown:border-black placeholder-shown:border-t-black focus:border-2 focus:border-black focus:border-t-transparent focus:outline-0 disabled:border-0 disabled:bg-blue-gray-50"
                 placeholder=" "
+                type="password"
+                name="password"
+                id="password"
+                value={signupState.password}
+                onChange={setSignupState}
               />
               <label className="before:content[' '] after:content[' '] pointer-events-none absolute left-0 -top-1.5 flex h-full w-full select-none text-[11px] font-normal leading-tight text-gray-400 transition-all before:pointer-events-none before:mt-[6.5px] before:mr-1 before:box-border before:block before:h-1.5 before:w-2.5 before:rounded-tl-md before:border-t before:border-l before:border-black before:transition-all after:pointer-events-none after:mt-[6.5px] after:ml-1 after:box-border after:block after:h-1.5 after:w-2.5 after:flex-grow after:rounded-tr-md after:border-t after:border-r after:border-black after:transition-all peer-placeholder-shown:text-sm peer-placeholder-shown:leading-[3.75] peer-placeholder-shown:text-gray-600 peer-placeholder-shown:before:border-transparent peer-placeholder-shown:after:border-transparent peer-focus:text-[11px] peer-focus:leading-tight peer-focus:text-gray-600 peer-focus:before:border-t-2 peer-focus:before:border-l-2 peer-focus:before:border-black peer-focus:after:border-t-2 peer-focus:after:border-r-2 peer-focus:after:border-black peer-disabled:text-transparent peer-disabled:before:border-transparent peer-disabled:after:border-transparent peer-disabled:peer-placeholder-shown:text-gray-600">
                 Password
@@ -53,7 +66,6 @@ export default function signup() {
             <div className="inline-flex items-center my-5">
               <label
                 className="relative flex cursor-pointer items-center rounded-full p-3"
-
                 data-ripple-dark="true"
               >
                 <input
@@ -78,29 +90,36 @@ export default function signup() {
                   </svg>
                 </div>
               </label>
-              <label
-                className="mt-px sm:text-xs md:text-sm cursor-pointer select-none font-light text-gray-950"
-
-              >
+              <label className="mt-px sm:text-xs md:text-sm cursor-pointer select-none font-light text-gray-950">
                 I agree to join swift invoice's mailing list
               </label>
             </div>
 
-            <button className='border-2 border-black rounded-lg w-[22.375rem] h-[3rem] bg-black text-white font-semibold'>Create account</button>
-            <div className='text-xs text-center text-gray-600 my-5'>
-              <span>By clicking "Create account" or "Continue with Google",<br />  you agree to the <span className='text-blue-600  cursor-pointer'  >Privacy Policy</span></span>
+            <button type="submit" className="border-2 border-black rounded-lg w-[22.375rem] h-[3rem] bg-black text-white font-semibold">
+              Create account
+            </button>
+            <div className="text-xs text-center text-gray-600 my-5">
+              <span>
+                By clicking "Create account" or "Continue with Google",
+                <br /> you agree to the{" "}
+                <span className="text-blue-600  cursor-pointer">
+                  Privacy Policy
+                </span>
+              </span>
             </div>
 
-            
-              
-              <div className='text-gray-700'> 
-                already have an account? <a onClick={()=>router?.push("/login")} className='text-blue-600  cursor-pointer'>Log in</a>
-              </div>
-            
+            <div className="text-gray-700">
+              already have an account?{" "}
+              <a
+                onClick={() => router?.push("/login")}
+                className="text-blue-600  cursor-pointer"
+              >
+                Log in
+              </a>
+            </div>
           </div>
-
         </div>
       </div>
     </>
-  )
+  );
 }
