7d3bfd16c0aa3671a94b740222230d551a2965bb
mohdyaserkt <yaserkt786@gmail.com>
2023-08-15 13:51:12 +0000

    15 Aug 2023, 19:21

--- src/app/(auth)/login/page.tsx
+++ src/app/(auth)/login/page.tsx
@@ -22,17 +22,17 @@ export default function login() {
       email: loginState.email as string,
       password: loginState.password as string,
     })
-      .then(({data}:any) => {
+      .then(({ data }: any) => {
         if (data) {
-          console.log(data.user,"sdfds");
-          localStorage.setItem("user",data.user as any)
-          router?.push("/getstarted")
-
+          console.log(data.user, "sdfds");
+          const user = data.user;
+          localStorage.setItem("user", user);
+          router?.push("/getstarted");
         }
       })
       .catch((err: ApiError) => {
         console.log(err.message);
-        alert(err)
+        alert(err);
       });
   };
   const [errors, setErrors] = useState<{
@@ -49,7 +49,9 @@ export default function login() {
           <div className="flex h-full justify-center items-center ">
             <div className="bg-white md:w-2/3 lg:w-1/3 lg:h-4/5 sm:w-full rounded sm:p-10  md:rounded-s-[5rem]  md:rounded-t-[5rem]  flex flex-col   items-center justify-center ">
               <div className="flex flex-col  items-center justify-center">
-                <div className="text-2xl font-ligthbold">Login To hawkinvoice</div>
+                <div className="text-2xl font-ligthbold">
+                  Login To hawkinvoice
+                </div>
                 <br />
                 <button className="mt-8 border-2 border-black rounded-lg w-[22.375rem] h-[3rem] flex justify-center items-center">
                   <div className="text-base inline-flex items-center gap-3">
@@ -66,9 +68,9 @@ export default function login() {
                   className="rounded-lg w-[22.375rem] h-[3rem]  peer   border  border-black border-t-transparent bg-transparent px-3 py-2.5  text-sm  text-black outline outline-1 transition-all placeholder-shown:border placeholder-shown:border-black placeholder-shown:border-t-black focus:border-2 focus:border-black focus:border-t-transparent focus:outline-0 disabled:border-0 disabled:bg-blue-gray-50"
                   placeholder=" "
                   name="email"
-                id="email"
-                value={loginState.email}
-                onChange={setLoginState}
+                  id="email"
+                  value={loginState.email}
+                  onChange={setLoginState}
                 />
                 <label className="before:content[' '] after:content[' '] pointer-events-none absolute left-0 -top-1.5 flex h-full w-full select-none text-[11px] font-normal leading-tight text-gray-400 transition-all before:pointer-events-none before:mt-[6.5px] before:mr-1 before:box-border before:block before:h-1.5 before:w-2.5 before:rounded-tl-md before:border-t before:border-l before:border-black before:transition-all after:pointer-events-none after:mt-[6.5px] after:ml-1 after:box-border after:block after:h-1.5 after:w-2.5 after:flex-grow after:rounded-tr-md after:border-t after:border-r after:border-black after:transition-all peer-placeholder-shown:text-sm peer-placeholder-shown:leading-[3.75] peer-placeholder-shown:text-gray-600 peer-placeholder-shown:before:border-transparent peer-placeholder-shown:after:border-transparent peer-focus:text-[11px] peer-focus:leading-tight peer-focus:text-gray-600 peer-focus:before:border-t-2 peer-focus:before:border-l-2 peer-focus:before:border-black peer-focus:after:border-t-2 peer-focus:after:border-r-2 peer-focus:after:border-black peer-disabled:text-transparent peer-disabled:before:border-transparent peer-disabled:after:border-transparent peer-disabled:peer-placeholder-shown:text-gray-600">
                   Email
