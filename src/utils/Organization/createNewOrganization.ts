6da148620bd93f309a81e2ea3e89aecca1948f7c
mohdyaserkt <yaserkt786@gmail.com>
2023-08-15 15:06:37 +0000

    15 Aug 2023, 20:36

--- src/utils/Organization/createNewOrganization.ts
+++ src/utils/Organization/createNewOrganization.ts
@@ -15,16 +15,20 @@ export const handleCreateNewOrganization = async ({
   setError,
 }: handleCreateNewOrganization): Promise<string | boolean | AxiosError> => {
   console.log("handle signup");
-
+    const user=JSON.parse(localStorage.getItem("user")as string)
   buisinessName = buisinessName.trim();
   typeOfbusiness = typeOfbusiness.trim();
   phoneNumber = phoneNumber.trim();
   NoOfemployes = NoOfemployes.trim();
   annualRevenue = annualRevenue.trim()
+  const email = user.email.trim();
+  const userId = user.userId.trim();
+
+
 
 
   try {
-    const tenant = await createNewOrganization({annualRevenue,buisinessName,NoOfemployes,phoneNumber,typeOfbusiness});
+    const tenant = await createNewOrganization({annualRevenue,buisinessName,NoOfemployes,phoneNumber,typeOfbusiness,email,userId});  
     return Promise.resolve(tenant as any);
   } catch (error) {
     const err = error as AxiosError;
