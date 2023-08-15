04973888cfa9c57a2d4c5a65d1defea411dfb563
mohdyaserkt <yaserkt786@gmail.com>
2023-08-15 04:03:53 +0000

    15 Aug 2023, 09:33

--- src/api/user/userAuth.ts
+++ src/api/user/userAuth.ts
@@ -17,3 +17,24 @@ export const register = async ({ email,password }:IUserCredentials): Promise<IUs
      return Promise.reject(error);
     }
   }
+
+
+
+
+  export const loginApi = async ({ email,password }:IUserCredentials): Promise<IUser|Error> => {
+    console.log("register");
+    
+    try {
+      const response = await axiosInstance.post(`/api/user/login/`, { email,password });
+    //   const { user, accessToken, refreshToken } = response.data as ResponseData;
+    //   localStorage.setItem("accessToken", accessToken as string);
+    //   localStorage.setItem("refreshToken", refreshToken as string);
+    //   localStorage.setItem("user", JSON.stringify(user));
+    //   store.dispatch(saveUser(user as User));
+      return Promise.resolve(response as any);
+    } catch (error) {
+        
+        
+     return Promise.reject(error);
+    }
+  }
---
+++ src/utils/Authentication/handleLogin.ts
@@ -0,0 +1,45 @@
+import { AxiosError } from "axios";
+import { validateEmail, validatePassword } from "../formValidators";
+import login from "@/app/(auth)/login/page";
+import { loginApi } from "@/api/user/userAuth";
+
+export const handleLogin = async ({
+  email,
+  password,
+  setError,
+}: IhandleSignup): Promise<string | boolean | AxiosError> => {
+  console.log("handle signup");
+
+  email = email.trim();
+  password = password.trim();
+
+  if (!validateEmail(email)) {
+    setError("email", ["Provide a valid email address"]);
+    return false;
+  }
+
+  if (!validatePassword(password)) {
+    console.log(
+      "At least 8 characters,One uppercase letterOne lowercase letterOne digit One special character"
+    );
+
+    setError("password", [
+      "At least 8 characters",
+      "One uppercase letter",
+      "One lowercase letter",
+      "One digit",
+      "One special character",
+    ]);
+    return false;
+  }
+
+  try {
+    const user = await loginApi({ email, password });
+    return Promise.resolve(user as any);
+  } catch (error) {
+    const err = error as AxiosError;
+    console.log(err, err.message, "error");
+
+    return Promise.reject(err.response?.data);
+  }
+};
