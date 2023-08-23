360d8539d1e667c5ab0788638825a39c6808e796
mohdyaserkt <yaserkt786@gmail.com>
2023-08-23 14:43:07 +0000

    23 Aug 2023, 20:13

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -5,33 +5,33 @@ import CredentialsProvider from "next-auth/providers/credentials";
 
 const handler = NextAuth({
   providers: [
-    // CredentialsProvider({
+    CredentialsProvider({
       
-    //   name: 'Credentials',
+      name: 'Credentials',
     
-    //   credentials: {
-    //     email: { label: "email", type: "email", placeholder: "email" },
-    //     password: { label: "Password", type: "password" }
-    //   },
-    //   async authorize(credentials, req) {
-    //     // You need to provide your own logic here that takes the credentials
-    //     // submitted and returns either a object representing a user or value
-    //     // that is false/null if the credentials are invalid.
-    //     // e.g. return { id: 1, name: 'J Smith', email: 'jsmith@example.com' }
-    //     // You can also use the `req` object to obtain additional parameters
-    //     // (i.e., the request IP address)
+      credentials: {
+        email: { label: "email", type: "email", placeholder: "email" },
+        password: { label: "Password", type: "password" }
+      },
+      async authorize(credentials, req) {
+        // You need to provide your own logic here that takes the credentials
+        // submitted and returns either a object representing a user or value
+        // that is false/null if the credentials are invalid.
+        // e.g. return { id: 1, name: 'J Smith', email: 'jsmith@example.com' }
+        // You can also use the `req` object to obtain additional parameters
+        // (i.e., the request IP address)
         
         
-    //     const user = await res.json()
+        // const user = await res.json()
   
-    //     // If no error and we have user data, return it
-    //     if (res.ok && user) {
-    //       return user
-    //     }
-    //     // Return null if user data could not be retrieved
-    //     return null
-    //   }
-    // }),
+        // // If no error and we have user data, return it
+        // if (res.ok && user) {
+        //   return user
+        // }
+        // Return null if user data could not be retrieved
+        return null
+      }
+    }),
     GoogleProvider({
       clientId: process.env.GOOGLE_CLIENT_ID?process.env.GOOGLE_CLIENT_ID:"",
       clientSecret: process.env.GOOGLE_CLIENT_SECRET?process.env.GOOGLE_CLIENT_SECRET:"",
