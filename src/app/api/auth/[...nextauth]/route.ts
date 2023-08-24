86311a6470b4896d28c2ad3d69202c2e85498b03
mohdyaserkt <yaserkt786@gmail.com>
2023-08-24 06:18:23 +0000

    24 Aug 2023, 11:48

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -2,28 +2,40 @@ import NextAuth, { RequestInternal } from "next-auth";
 import GoogleProvider from "next-auth/providers/google";
 import CredentialsProvider from "next-auth/providers/credentials";
 import { loginApi } from "../../user/userAuth";
+import { ApiError } from "next/dist/server/api-utils";
 
 const handler = NextAuth({
   providers: [
     CredentialsProvider({
-      name: 'Credentials',
-      
+      name: "Credentials",
+
       credentials: {
         email: { label: "Email", type: "email", placeholder: "email" },
-        password: { label: "Password", type: "password" }
+        password: { label: "Password", type: "password" },
       },
       async authorize(credentials, req) {
-        let email:string=credentials?.email||""
-        let password:string=credentials?.password||""
-        let user = await loginApi({email,password});
-  
-        // If no error and we have user data, return it
-        // if ( user) {
-        //   return user
-        // }
+        let email: string = credentials?.email || "";
+        let password: string = credentials?.password || "";
+        let userdata;
+        let error;
+        let user = await loginApi({ email, password })
+          .then(({ data }: any) => {
+            if (data) {
+              localStorage.setItem("user", JSON.stringify(data.user));
+              userdata = data.user;
+            }
+          })
+          .catch((err: ApiError) => {
+            console.log(err.message);
+            error = err;
+            alert(err);
+          });
+        if (userdata) {
+          return userdata;
+        }
         // Return null if user data could not be retrieved
-        return null
-      }
+        return error ? error : null;
+      },
     }),
     GoogleProvider({
       clientId: process.env.GOOGLE_CLIENT_ID
