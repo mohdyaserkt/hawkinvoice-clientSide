03acd668b60ab7b4c3acfb6f855002f00a04574b
mohdyaserkt <yaserkt786@gmail.com>
2023-08-27 09:43:17 +0000

    27 Aug 2023, 15:13

--- src/app/(cutomer)/customers/page.tsx
+++ src/app/(cutomer)/customers/page.tsx
@@ -5,19 +5,21 @@ import React, { useState } from "react";
 import { useRouter } from "next/navigation";
 import { ApiError } from "next/dist/server/api-utils";
 import { LiaFileInvoiceDollarSolid } from 'react-icons/lia';
-import {  } from "react-icons/bi";
+import { MdRefresh } from "react-icons/md";
 
 
 const getStarted = () => {
   return (
     <>
       <div className="h-screen">
-        <nav>
+        <nav className="flex items-center space-x-3">
           <div className="flex items-center space-x-2">
           <LiaFileInvoiceDollarSolid className="text-white w-8 h-8" />
           <h3 className="text-white text-lg">HawkInvoice</h3>
           </div>
           <div>
+          <MdRefresh className="text-white w-5 h-5" />
+          
             
 
           </div>
