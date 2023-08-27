0ff15be0dcee29299a761d0b201bf5d2b09b94cb
mohdyaserkt <yaserkt786@gmail.com>
2023-08-27 11:48:39 +0000

    27 Aug 2023, 17:18

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,27 +1,35 @@
+const withMT = require("@material-tailwind/react/utils/withMT");
+ 
+
 /** @type {import('tailwindcss').Config} */
-module.exports = {
-  content: [
-    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
-  ],
-  theme: {
-    extend: {
-      colors: {
-        primary: "#143230",
-        secondary: "#1FE290",    
+
+module.exports = withMT(
+  {
+    content: [
+      "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
+      "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+      "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+    ],
+    theme: {
+      extend: {
+        colors: {
+          primary: "#143230",
+          secondary: "#1FE290",    
+        },
+        screens: {
+          'sm': '350px', // Change the default width from 640px to 480px
+          'md': '768px',
+          'lg': '1024px',
+          'xl': '1280px',
+        },
+        fontWeight:{
+          "ligthbold":"400"
+        }
+        
       },
-      screens: {
-        'sm': '350px', // Change the default width from 640px to 480px
-        'md': '768px',
-        'lg': '1024px',
-        'xl': '1280px',
-      },
-      fontWeight:{
-        "ligthbold":"400"
-      }
-      
     },
-  },
-  plugins: [],
-};
+    plugins: [],
+  }
+);
+
+
