c48b03733bc68bb3e86e1df906a1d3446c4efee2
mohdyaserkt <yaserkt786@gmail.com>
2023-08-27 10:28:42 +0000

    27 Aug 2023, 15:58

--- src/app/(cutomer)/customers/page.tsx
+++ src/app/(cutomer)/customers/page.tsx
@@ -4,9 +4,9 @@ import HandleForm from "@/utils/handleFormState";
 import React, { useState } from "react";
 import { useRouter } from "next/navigation";
 import { ApiError } from "next/dist/server/api-utils";
-import { LiaFileInvoiceDollarSolid } from 'react-icons/lia';
+import { LiaFileInvoiceDollarSolid } from "react-icons/lia";
 import { MdRefresh } from "react-icons/md";
-
+import { AiOutlineSearch } from "react-icons/ai";
 
 const getStarted = () => {
   return (
@@ -14,28 +14,27 @@ const getStarted = () => {
       <div className="h-screen">
         <nav className="flex items-center space-x-3">
           <div className="flex items-center space-x-2">
-          <LiaFileInvoiceDollarSolid className="text-white w-8 h-8" />
-          <h3 className="text-white text-lg">HawkInvoice</h3>
+            <LiaFileInvoiceDollarSolid className="text-white w-8 h-8" />
+            <h3 className="text-white text-lg">HawkInvoice</h3>
           </div>
           <div>
-            <div>
-               <MdRefresh className="text-white w-5 h-5" />
-               <div>
+            <div className=" flex items-center">
+              <MdRefresh className="text-white w-5 h-5" />
+              <div className="flex items-center  bg-secondary p-1 px-2 rounded-lg border-secondary border">
+                <AiOutlineSearch className="text-black w-5 h-5" />
+                <span className="text-gray-900 text-lg mx-2">|</span>
                 <div>
-
+                  <input
+                    type="text"
+                    placeholder="Search in Customers(/)"
+                    className="bg-transparent text-black border-none focus:outline-none placeholder-black text-xs"
+                  />
                 </div>
-                
-               </div>
+              </div>
             </div>
-         
-
-            
-
           </div>
         </nav>
-        <div className="">
-          
-        </div>
+        <div className=""></div>
       </div>
     </>
   );
