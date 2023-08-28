59bd8bb9387ac8627ef5e81d4d51d7c801336842
mohdyaserkt <yaserkt786@gmail.com>
2023-08-28 05:11:06 +0000

    28 Aug 2023, 10:41

--- src/app/(cutomer)/addnewcustomer/page.tsx
+++ src/app/(cutomer)/addnewcustomer/page.tsx
@@ -1,18 +1,116 @@
 "use client";
-
 import React, { useState } from "react";
+import {
+  LiaFileInvoiceDollarSolid,
+  LiaFileInvoiceSolid,
+} from "react-icons/lia";
+import { MdRefresh } from "react-icons/md";
+import { PiHandbag } from "react-icons/pi";
+import { BsPlusSquareFill, BsFileEarmarkBarGraph } from "react-icons/bs";
+import { TbReceipt } from "react-icons/tb";
+import { SlOptions } from "react-icons/sl";
+import {
+  AiOutlineSetting,
+  AiOutlineHome,
+  AiOutlineUser,
+  AiOutlineSearch,
+} from "react-icons/ai";
+
 
 const getStarted = () => {
   return (
     <>
-      <form>
-        <div className="h-screen">
-          <nav></nav>
-          <div className="flex h-full justify-center items-center ">
+      <div className="h-screen">
+        <nav className="flex items-center space-x-3">
+          <div className="flex items-center space-x-2 w-52  mr-1">
+            <LiaFileInvoiceDollarSolid className="text-white w-8 h-8" />
+            <h3 className="text-white text-lg">HawkInvoice</h3>
+          </div>
+          <div className="flex justify-between items-center border border-white w-full">
+            <div className=" flex items-center">
+              <MdRefresh className="text-white w-5 h-5 m-3" />
+              <div className="flex items-center  bg-secondary p-1 px-2 m-1 rounded-lg border-secondary border lg:h-8">
+                <AiOutlineSearch className="text-black w-5 h-5" />
+                <span className="text-gray-900 text-lg mx-2">|</span>
+                <div className="mr-[10px]">
+                  <input
+                    type="text"
+                    placeholder="Search in Customers(/)"
+                    className="bg-transparent text-black border-none focus:outline-none placeholder-black text-xs"
+                  />
+                </div>
+              </div>
+            </div>
+
+            <div className="flex items-center space-x-2">
+              <BsPlusSquareFill className="bg-black text-secondary" />
+              <span className=" text-lg text-white ">|</span>
+              <AiOutlineSetting className=" text-white" />
+              <span className=" text-lg text-white ">|</span>
+              <select className="">
+                <option>Organization</option>
+                <option>Organization 2</option>
+                <option>Organization 3</option>
+              </select>
+              <div className="pr-2">
+                <img
+                  src="https://img.freepik.com/free-psd/engraved-black-logo-mockup_125540-223.jpg?w=900&t=st=1693152334~exp=1693152934~hmac=da365a4885d210047abff64bf790f521687c842a32793b5c0416be75b321f977"
+                  alt="Your Alt Text"
+                  className="rounded-full w-8   h-8   "
+                />
+              </div>
+            </div>
+          </div>
+        </nav>
+        <div className="flex">
+          <div className=" w-56 flex flex-col gap-3 py-3 px-2 border ">
+            <div className="flex items-center  h-7 rounded-lg space-x-2">
+              <AiOutlineHome className="w-4 h-4 ml-2 text-white" />
+              <p  className="text-sm text-white" >Home</p>
+            </div>
+            <div className="flex items-center bg-secondary h-7 rounded-lg space-x-2">
+              <AiOutlineUser className="w-4 h-4 ml-2 text-white" />
+              <p  className="text-sm text-white">Customers</p>
+            </div>
+            <div className="flex items-center h-7 rounded-lg space-x-2">
+              <PiHandbag className="w-4 h-4 ml-2 text-white " />
+              <p  className="text-sm text-white">Items</p>
+            </div>
+            <div className="flex items-center h-7 rounded-lg space-x-2">
+              <LiaFileInvoiceSolid className="w-4 h-4 ml-2 text-white" />
+              <p  className="text-sm text-white">Invoices</p>
+            </div>
+            <div className="flex items-center h-7 rounded-lg space-x-2">
+              <img src="/recievedpayments.svg" alt="" className="w-4 h-4 ml-2 text-white" />
+              <p  className="text-sm text-white">Payments Recieved</p>
+            </div>
+            <div className="flex items-center h-7 rounded-lg space-x-2">
+              <TbReceipt className="w-4 h-4 ml-2 text-white" />
+              <p  className="text-sm text-white">Expenses</p>
+            </div>
+            <div className="flex items-center h-7 rounded-lg space-x-2">
+              <BsFileEarmarkBarGraph className="w-4 h-4 ml-2 text-white" />
+              <p  className="text-sm text-white">Reports</p>
+            </div>
+          </div>
+          <div className="w-full">
+            <div className="p-5 flex justify-between w-full border-r-[1px] border-b-[1px] ">
+              <div className="text-white">
+                <h2 className="text-xl">New Customer</h2>
+              </div>
+            </div>
+
+            <div>
+              <div>
+                <form action="">
+                  
+                </form>
+              </div>
+            </div>
             
           </div>
         </div>
-      </form>
+      </div>
     </>
   );
 };
