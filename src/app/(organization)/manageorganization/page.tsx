323b5afab5bae66097e3c570e1a53bc7fabc2ed3
mohdyaserkt <yaserkt786@gmail.com>
2023-08-30 14:59:03 +0000

    30 Aug 2023, 20:29

--- src/app/(organization)/manageorganization/page.tsx
+++ src/app/(organization)/manageorganization/page.tsx
@@ -71,7 +71,7 @@ const manageOrganisation = () => {
                   <div className="flex justify-between">
                     <h1 className="text-base text-white">{item.businessName}</h1>
                     <div className="flex justify-center">
-                    <Link href={`http://${item.businessName}.hawkinvoice.com/login`}><button className=" border-[1px] border-blue-500 text-blue-500 text-xs p-2  rounded">
+                    <Link href={`http://${item.businessName}.hawkinvoice.com/customers`}><button className=" border-[1px] border-blue-500 text-blue-500 text-xs p-2  rounded">
                         Go to Organization
                       </button></Link>
                       <button className="bg-white border-2 rounded text-base ml-2 px-1  ">
