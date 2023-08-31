59c22ffe3aa34f8a65535c9df4dc67cded163977
mohdyaserkt <yaserkt786@gmail.com>
2023-08-31 09:55:22 +0000

    31 Aug 2023, 15:25

--- src/app/(cutomer)/addnewcustomer/page.tsx
+++ src/app/(cutomer)/addnewcustomer/page.tsx
@@ -1,129 +1,129 @@
 "use client";
 import React, { useState } from "react";
-import {
-  LiaFileInvoiceDollarSolid,
-  LiaFileInvoiceSolid,
-} from "react-icons/lia";
-import { MdRefresh } from "react-icons/md";
-import { PiHandbag, PiCopySimple } from "react-icons/pi";
-import { BsPlusSquareFill, BsFileEarmarkBarGraph } from "react-icons/bs";
-import { TbReceipt } from "react-icons/tb";
-import { SlOptions } from "react-icons/sl";
-import {
-  AiOutlineSetting,
-  AiOutlineHome,
-  AiOutlineUser,
-  AiOutlineSearch,
-} from "react-icons/ai";
-import { handleCreateNewCustomer } from "@/utils/Customers/createNewCustomer";
+// import {
+//   LiaFileInvoiceDollarSolid,
+//   LiaFileInvoiceSolid,
+// } from "react-icons/lia";
+// import { MdRefresh } from "react-icons/md";
+// import { PiHandbag, PiCopySimple } from "react-icons/pi";
+// import { BsPlusSquareFill, BsFileEarmarkBarGraph } from "react-icons/bs";
+// import { TbReceipt } from "react-icons/tb";
+// // import { SlOptions } from "react-icons/sl";
+// import {
+//   AiOutlineSetting,
+//   AiOutlineHome,
+//   AiOutlineUser,
+//   AiOutlineSearch,
+// } from "react-icons/ai";
+// import { handleCreateNewCustomer } from "@/utils/Customers/createNewCustomer";
 import { useRouter } from "next/navigation";
-import { ApiError } from "next/dist/server/api-utils";
+// import { ApiError } from "next/dist/server/api-utils";
 
 const getStarted = () => {
   const router = useRouter();
-  const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
-    e.preventDefault();
-    const form = e.target as HTMLFormElement;
-    const [
-      custumerTypeBusinessInput,
-      custumerTypeIndividualInput,
-      salutationInput,
-      firstNameInput,
-      lastNameInput,
-      companyNameInput,
-      DisplayNameInput,
-      EmailInput,
-      WorkPhoneInput,
-      MobileInput,
-      billingAddressAttention,
-      billingAddressRegion,
-      billingAddressStreet1,
-      billingAddressStreet2,
-      billingAddressCity,
-      billingAddressState,
-      shippingAddressAttention,
-      shippingAddressRegion,
-      shippingAddressStreet1,
-      shippingAddressStreet2,
-      shippingAddressCity,
-      shippingAddressState,
-    ] = Array.from(form.elements) as HTMLInputElement[];
+  // const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
+  //   e.preventDefault();
+  //   const form = e.target as HTMLFormElement;
+  //   const [
+  //     custumerTypeBusinessInput,
+  //     custumerTypeIndividualInput,
+  //     salutationInput,
+  //     firstNameInput,
+  //     lastNameInput,
+  //     companyNameInput,
+  //     DisplayNameInput,
+  //     EmailInput,
+  //     WorkPhoneInput,
+  //     MobileInput,
+  //     billingAddressAttention,
+  //     billingAddressRegion,
+  //     billingAddressStreet1,
+  //     billingAddressStreet2,
+  //     billingAddressCity,
+  //     billingAddressState,
+  //     shippingAddressAttention,
+  //     shippingAddressRegion,
+  //     shippingAddressStreet1,
+  //     shippingAddressStreet2,
+  //     shippingAddressCity,
+  //     shippingAddressState,
+  //   ] = Array.from(form.elements) as HTMLInputElement[];
 
-    const customer = {
-      customerType:
-        custumerTypeBusinessInput.value == "on" ? "business" : "individual",
-      salutation: salutationInput.value,
-      firstName: firstNameInput.value,
-      lastName: lastNameInput.value,
-      customerCompanyName: companyNameInput.value,
-      displayName: DisplayNameInput.value,
-      email: EmailInput.value,
-      workPhone: WorkPhoneInput.value as unknown as number,
-      mobile: MobileInput.value as unknown as number,
-      billingAddress: {
-        attention: billingAddressAttention.value,
-        region: billingAddressRegion.value,
-        street1: billingAddressStreet1.value,
-        street2: billingAddressStreet2.value,
-        city: billingAddressCity.value,
-        state: billingAddressState.value,
-      },
-      shippingAddress: {
-        attention: shippingAddressAttention.value,
-        region: shippingAddressRegion.value,
-        street1: shippingAddressStreet1.value,
-        street2: shippingAddressStreet2.value,
-        city: shippingAddressCity.value,
-        state: shippingAddressState.value,
-      },
-    };
-    console.log(customer);
-
-
-
-    handleCreateNewCustomer({
-      customer,
-      setError
-
-      // businessName: organizationState.businessName as string,
-      // typeOfbusiness: organizationState.typeOfbusiness as string,
-    })
-      .then((res: any) => {
-        if (res) {
-          console.log(res);
-          router?.push("/customers");
-          alert(res);
-        }
-      })  
-      .catch((err: ApiError) => {
-        console.log(err.message);
-        alert(err);
-      });
+    // const customer = {
+    //   customerType:
+    //     custumerTypeBusinessInput.value == "on" ? "business" : "individual",
+    //   salutation: salutationInput.value,
+    //   firstName: firstNameInput.value,
+    //   lastName: lastNameInput.value,
+    //   customerCompanyName: companyNameInput.value,
+    //   displayName: DisplayNameInput.value,
+    //   email: EmailInput.value,
+    //   workPhone: WorkPhoneInput.value as unknown as number,
+    //   mobile: MobileInput.value as unknown as number,
+    //   billingAddress: {
+    //     attention: billingAddressAttention.value,
+    //     region: billingAddressRegion.value,
+    //     street1: billingAddressStreet1.value,
+    //     street2: billingAddressStreet2.value,
+    //     city: billingAddressCity.value,
+    //     state: billingAddressState.value,
+    //   },
+    //   shippingAddress: {
+    //     attention: shippingAddressAttention.value,
+    //     region: shippingAddressRegion.value,
+    //     street1: shippingAddressStreet1.value,
+    //     street2: shippingAddressStreet2.value,
+    //     city: shippingAddressCity.value,
+    //     state: shippingAddressState.value,
+    //   },
+    // };
+    // console.log(customer);
 
 
 
+    // handleCreateNewCustomer({
+    //   customer,
+    //   setError
+
+    //   // businessName: organizationState.businessName as string,
+    //   // typeOfbusiness: organizationState.typeOfbusiness as string,
+    // })
+    //   .then((res: any) => {
+    //     if (res) {
+    //       console.log(res);
+    //       router?.push("/customers");
+    //       alert(res);
+    //     }
+    //   })  
+    //   .catch((err: ApiError) => {
+    //     console.log(err.message);
+    //     alert(err);
+    //   });
 
 
-  };
-  const [errors, setErrors] = useState<{
-    field: string;
-    errors: string[];
-  } | null>({ field: "", errors: [""] });
-  const setError = (field: string, errorMessages: string[]) =>
-    setErrors({ field, errors: errorMessages });
+
+
+
+  // };
+  // const [errors, setErrors] = useState<{
+  //   field: string;
+  //   errors: string[];
+  // } | null>({ field: "", errors: [""] });
+  // const setError = (field: string, errorMessages: string[]) =>
+  //   setErrors({ field, errors: errorMessages });
   return (
     <>
       <div className="h-screen">
         <nav className="flex items-center space-x-3">
           <div className="flex items-center space-x-2 w-52  mr-1">
-            <LiaFileInvoiceDollarSolid className="text-white w-8 h-8" />
+            {/* <LiaFileInvoiceDollarSolid className="text-white w-8 h-8" /> */}
             <h3 className="text-white text-lg">HawkInvoice</h3>
           </div>
           <div className="flex justify-between items-center border border-white w-full">
             <div className=" flex items-center">
-              <MdRefresh className="text-white w-5 h-5 m-3" />
+              {/* <MdRefresh className="text-white w-5 h-5 m-3" /> */}
               <div className="flex items-center  bg-secondary p-1 px-2 m-1 rounded-lg border-secondary border lg:h-8">
-                <AiOutlineSearch className="text-black w-5 h-5" />
+                {/* <AiOutlineSearch className="text-black w-5 h-5" /> */}
                 <span className="text-gray-900 text-lg mx-2">|</span>
                 <div className="mr-[10px]">
                   <input
@@ -136,9 +136,9 @@ const getStarted = () => {
             </div>
 
             <div className="flex items-center space-x-2">
-              <BsPlusSquareFill className="bg-black text-secondary" />
+              {/* <BsPlusSquareFill className="bg-black text-secondary" /> */}
               <span className=" text-lg text-white ">|</span>
-              <AiOutlineSetting className=" text-white" />
+              {/* <AiOutlineSetting className=" text-white" /> */}
               <span className=" text-lg text-white ">|</span>
               <select className="">
                 <option>Organization</option>
@@ -158,19 +158,19 @@ const getStarted = () => {
         <div className="flex">
           <div className=" w-56 flex flex-col gap-3 py-3 px-2 border ">
             <div className="flex items-center  h-7 rounded-lg space-x-2">
-              <AiOutlineHome className="w-4 h-4 ml-2 text-white" />
+              {/* <AiOutlineHome className="w-4 h-4 ml-2 text-white" /> */}
               <p className="text-sm text-white">Home</p>
             </div>
             <div className="flex items-center bg-secondary h-7 rounded-lg space-x-2">
-              <AiOutlineUser className="w-4 h-4 ml-2 text-white" />
+              {/* <AiOutlineUser className="w-4 h-4 ml-2 text-white" /> */}
               <p className="text-sm text-white">Customers</p>
             </div>
             <div className="flex items-center h-7 rounded-lg space-x-2">
-              <PiHandbag className="w-4 h-4 ml-2 text-white " />
+              {/* <PiHandbag className="w-4 h-4 ml-2 text-white " /> */}
               <p className="text-sm text-white">Items</p>
             </div>
             <div className="flex items-center h-7 rounded-lg space-x-2">
-              <LiaFileInvoiceSolid className="w-4 h-4 ml-2 text-white" />
+              {/* <LiaFileInvoiceSolid className="w-4 h-4 ml-2 text-white" /> */}
               <p className="text-sm text-white">Invoices</p>
             </div>
             <div className="flex items-center h-7 rounded-lg space-x-2">
@@ -182,11 +182,11 @@ const getStarted = () => {
               <p className="text-sm text-white">Payments Recieved</p>
             </div>
             <div className="flex items-center h-7 rounded-lg space-x-2">
-              <TbReceipt className="w-4 h-4 ml-2 text-white" />
+              {/* <TbReceipt className="w-4 h-4 ml-2 text-white" /> */}
               <p className="text-sm text-white">Expenses</p>
             </div>
             <div className="flex items-center h-7 rounded-lg space-x-2">
-              <BsFileEarmarkBarGraph className="w-4 h-4 ml-2 text-white" />
+              {/* <BsFileEarmarkBarGraph className="w-4 h-4 ml-2 text-white" /> */}
               <p className="text-sm text-white">Reports</p>
             </div>
           </div>
@@ -200,7 +200,7 @@ const getStarted = () => {
             <div className="pt-7 pl-16">
               <div>
                 <form
-                  onSubmit={handleSubmit}
+                  // onSubmit={handleSubmit}
                   className="text-[13px] text-white"
                 >
                   <div className="flex flex-col gap-4">
@@ -364,7 +364,7 @@ const getStarted = () => {
                           Shipping Address
                         </h3>
                         <div className=" flex items-center text-secondary gap-2">
-                          <PiCopySimple />
+                          {/* <PiCopySimple /> */}
                           <p>Copy billing address</p>
                         </div>
                       </div>
