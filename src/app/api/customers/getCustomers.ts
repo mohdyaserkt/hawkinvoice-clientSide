7d2350b4016a9eb593ac7f292da9009ae29861f4
mohdyaserkt <yaserkt786@gmail.com>
2023-08-31 04:35:56 +0000

    31 Aug 2023, 10:05

--- src/app/api/customers/getCustomers.ts
+++ src/app/api/customers/getCustomers.ts
@@ -5,7 +5,7 @@ export const getCustomers = async (): Promise<IUser | Error> => {
   console.log("reached");
 
   try {
-    const response = await axiosInstance.post(`/api/customer/get-customers`);
+    const response = await axiosInstance.get(`/api/customer/get-customers`);
 
     return Promise.resolve(response as any);
   } catch (error) {
