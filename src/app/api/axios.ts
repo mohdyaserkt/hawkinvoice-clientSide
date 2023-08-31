6fde6ff86d8959b0082aa52c3846a4f7a4bda9e5
mohdyaserkt <yaserkt786@gmail.com>
2023-08-31 14:24:32 +0000

    31 Aug 2023, 19:54

--- src/app/api/axios.ts
+++ src/app/api/axios.ts
@@ -1,7 +1,28 @@
-import axios,{AxiosInstance} from "axios"
+import axios,{AxiosError, AxiosInstance} from "axios"
 const  axiosInstance : AxiosInstance= axios.create({
     baseURL : "https://hai.hawkinvoice.com"
   })
+
+  axiosInstance.interceptors.request.use(
+    (config) => {
+        const User =JSON.parse(localStorage.getItem("user") as string )
+        if(User) {
+        const AccessToken =User.AccessToken
+        console.log(AccessToken);
+        if(AccessToken){
+            config.headers["Authorization"] = `Bearer ${AccessToken}`;
+        }
+    }
+    
+    
+     return config
+    },
+    (error:AxiosError)=>{
+        return Promise.reject(error)
+    }
+);
+
+export { axiosInstance };
   
   
   export default axiosInstance
