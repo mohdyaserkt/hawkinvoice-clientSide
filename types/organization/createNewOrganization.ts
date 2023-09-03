cee5f58659a99723059710228d81f3603026686e
mohdyaserkt <yaserkt786@gmail.com>
2023-09-03 13:57:23 +0000

    3 Sept 2023, 19:27

--- types/organization/createNewOrganization.ts
+++ types/organization/createNewOrganization.ts
@@ -6,17 +6,31 @@ interface handleCreateNewOrganization {
   phoneNumber?: Number;
   NoOfemployes?: string;
   annualRevenue?: string;
+  organizationAddress?:{
+    address:string;
+    apt: string;
+    city: string;
+    state: string;
+    zipCode: Number;
+  }
 
   setError: (field: string, errors: string[]) => void;
 }
 
 
 interface IOrganizationData{
-    userId?: string;
+  userId?: string;
   businessName: string;
   email?: string;
   typeOfbusiness: string;
   phoneNumber?: Number;
   NoOfemployes?: string;
   annualRevenue?: string;
+  organizationAddress?:{
+    address:string;
+    apt: string;
+    city: string;
+    state: string;
+    zipCode: Number;
+  }
   }
