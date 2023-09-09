cf0d0e98bf9b7d80d53f802e01c836868a9150b9
mohdyaserkt <yaserkt786@gmail.com>
2023-09-09 12:24:12 +0000

    9 Sept 2023, 17:54

--- src/app/(settings)/orgprofile/page.tsx
+++ src/app/(settings)/orgprofile/page.tsx
@@ -99,12 +99,31 @@ const getStarted = () => {
                       <div>
                         <p>Your Logo</p>
                       </div>
-                      <div>
-                      <input
-          type="file"
-          accept="image/*" // Optional: Restrict file types to images
-          // onChange={this.handleFileUpload}
-        />
+                      <div className="bg-white w-[250px] h-20 flex justify-center items-center rounded">
+                        <div className="flex items-center gap-2">
+                          
+                          <svg
+                            xmlns="http://www.w3.org/2000/svg"
+                            fill="none"
+                            viewBox="0 0 24 24"
+                            strokeWidth="1.5"
+                            stroke="currentColor"
+                            className="w-4 h-4 text-slate-600"
+                          >
+                            <path
+                              stroke-linecap="round"
+                              stroke-linejoin="round"
+                              d="M3 16.5v2.25A2.25 2.25 0 005.25 21h13.5A2.25 2.25 0 0021 18.75V16.5m-13.5-9L12 3m0 0l4.5 4.5M12 3v13.5"
+                            />
+                          </svg>
+
+                          <p className="text-xs">Upload lour logo</p>
+                        </div>
+                        {/* <input
+                          type="file"
+                          accept="image/*" // Optional: Restrict file types to images
+                          // onChange={this.handleFileUpload}
+                        /> */}
                       </div>
                     </div>
                   </div>
