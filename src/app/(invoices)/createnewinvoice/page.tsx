8afeceacde33fe8643605eecd09f220a4f3deda2
mohdyaserkt <yaserkt786@gmail.com>
2023-09-10 11:51:03 +0000

    10 Sept 2023, 17:21

--- src/app/(invoices)/createnewinvoice/page.tsx
+++ src/app/(invoices)/createnewinvoice/page.tsx
@@ -111,16 +111,31 @@ const getStarted = () => {
             <div className="pt-7 pl-16">
               <div>
                 <form className="text-[13px] text-white">
-
+                  <div>
                     <div>
+                      <p>
+                        Customer Name<span className="text-red-500">*</span>
+                      </p>
+                      <div>
                         <div>
-                            <p>Customer Name<span className="text-red-500">*</span></p>
-                            
+                          <label className="sr-only">Underline select</label>
+                          <select
+                            id="underline_select"
+                            className="block py-2.5 px-0 w-full text-sm text-gray-500 bg-transparent border-0 border-b-2 border-gray-200 appearance-none dark:text-gray-400 dark:border-gray-700 focus:outline-none focus:ring-0 focus:border-gray-200 peer"
+                          >
+                            <option selected>Choose a country</option>
+                            <option value="US">United States</option>
+                            <option value="CA">Canada</option>
+                            <option value="FR">France</option>
+                            <option value="DE">Germany</option>
+                          </select>
                         </div>
+                        <div>0</div>
+                      </div>
                     </div>
 
                     
-                  
+                  </div>
                 </form>
               </div>
             </div>
