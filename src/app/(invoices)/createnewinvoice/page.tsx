7df9af21c05918c91a13bd3d81c786b52336f897
mohdyaserkt <yaserkt786@gmail.com>
2023-09-12 10:40:39 +0000

    12 Sept 2023, 16:10

--- src/app/(invoices)/createnewinvoice/page.tsx
+++ src/app/(invoices)/createnewinvoice/page.tsx
@@ -244,20 +244,21 @@ const getStarted = () => {
                           {items.map((item, index) => (
                             <tr className="border h-16" key={index}>
                               <td className="text-start pl-1">
-                              <textarea placeholder=" Type or click to select an item." className="border-none bg-transparent rounded-md text-xs w-64 p-2 focus:outline-none placeholder:text-white">
-                               
-                               </textarea>
-                               <div className="absolute bg-primary text-black rounded-md w-80 p-5 border  flex flex-col gap-3 ml-[-6px] mt-4 ">
-                                 <div className="text-secondary border rounded-md hover:text-blue-500">
+                                <textarea
+                                  placeholder=" Type or click to select an item."
+                                  className="border-none bg-transparent rounded-md text-xs w-64 p-2 focus:outline-none placeholder:text-white"
+                                ></textarea>
+                                <div className="absolute bg-primary text-black rounded-md w-80 p-5 border  flex flex-col gap-3 ml-[-6px] mt-4 ">
+                                  <div className="text-secondary border rounded-md hover:text-blue-500">
                                     <h1 className="font-bold p-3 ">New Item</h1>
-                                 </div>
-                                 <div className="text-secondary border rounded-md hover:text-blue-500">
+                                  </div>
+                                  <div className="text-secondary border rounded-md hover:text-blue-500">
                                     <h1 className="font-bold p-3">New Item</h1>
-                                 </div>
-                                 <div className="text-secondary border rounded-md hover:text-blue-500">
+                                  </div>
+                                  <div className="text-secondary border rounded-md hover:text-blue-500">
                                     <h1 className="font-bold p-3">New Item</h1>
-                                 </div>
-                               </div>
+                                  </div>
+                                </div>
                               </td>
                               <td className="text-center">
                                 <input
@@ -283,19 +284,20 @@ const getStarted = () => {
                             </tr>
                           ))}
                           <tr className="border h-16">
-                            <td className="text-start pl-1 relative">
-                              <textarea placeholder=" Type or click to select an item." className="border-none bg-transparent rounded-md text-xs w-64 p-2 focus:outline-none placeholder:text-white">
-                               
-                              </textarea>
+                            <td className="text-start pl-1 ">
+                              <textarea
+                                placeholder=" Type or click to select an item."
+                                className="border-none bg-transparent rounded-md text-xs w-64 p-2 focus:outline-none placeholder:text-white"
+                              ></textarea>
                               <div className="absolute bg-primary text-black rounded-md w-80 p-5 border   flex-col gap-3 ml-[-6px] mt-4 hidden ">
                                 <div className="text-secondary border rounded-md hover:text-blue-500">
-                                   <h1 className="font-bold p-3 ">New Item</h1>
+                                  <h1 className="font-bold p-3 ">New Item</h1>
                                 </div>
                                 <div className="text-secondary border rounded-md hover:text-blue-500">
-                                   <h1 className="font-bold p-3">New Item</h1>
+                                  <h1 className="font-bold p-3">New Item</h1>
                                 </div>
                                 <div className="text-secondary border rounded-md hover:text-blue-500">
-                                   <h1 className="font-bold p-3">New Item</h1>
+                                  <h1 className="font-bold p-3">New Item</h1>
                                 </div>
                               </div>
                             </td>
