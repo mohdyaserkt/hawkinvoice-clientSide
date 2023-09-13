c0498659722b0323faa52ad94ffdc93aa8367ff7
mohdyaserkt <yaserkt786@gmail.com>
2023-09-13 07:55:31 +0000

    13 Sept 2023, 13:25

--- src/app/(invoices)/createnewinvoice/page.tsx
+++ src/app/(invoices)/createnewinvoice/page.tsx
@@ -229,7 +229,7 @@ const getStarted = () => {
                         </div>
                       </div>
                     </div>
-                    <hr />
+                    <hr  className="w-7/12"/>
                     <div className="flex gap-[99px] items-center">
                       <div>
                         <p className="text-white text-sm">Salesperson</p>
@@ -241,7 +241,7 @@ const getStarted = () => {
                         />
                       </div>
                     </div>
-                    <hr />
+                    <hr  className="w-7/12"/>
                     <div className="flex gap-[132px] items-center">
                       <div>
                         <p className="text-white text-sm">Subject</p>
@@ -333,18 +333,55 @@ const getStarted = () => {
                           </div>
                           <p>0.00</p>
                         </div>
-                        <hr />
+                        <hr  className="w-7/12"/>
                         <div className="flex justify-between text-base">
                           <h2>Total ( ₹ ) </h2>
                           <h2>0.00</h2>
                         </div>
                       </div>
                     </div>
+                    <hr  className="w-7/12"/>
                     <div>
-                      <div className="flex gap-1">
-                        <input type="checkbox" className="" />{" "}
-                        <p>I have received the payment</p>
+                      <div className="inline-flex items-center ">
+                        <label
+                          className="relative flex cursor-pointer  items-center rounded-full p-2"
+                          data-ripple-dark="true"
+                        >
+                          <input
+                            id="login"
+                            // checked={isChecked}
+                            type="checkbox"
+                            // onChange={() => setIsChecked(!isChecked)}
+                            className="before:content[''] peer relative sm:w-3 sm:h-3 md:h-4 md:w-4 cursor-pointer appearance-none rounded border border-white transition-all before:absolute before:top-2/4 before:left-2/4 before:block before:h-12 before:w-12 before:-translate-y-2/4 before:-translate-x-2/4 before:rounded-full before:bg-blue-gray-500 before:opacity-0 before:transition-opacity checked:border-black  checked:bg-black checked:before:bg-black hover:before:opacity-10"
+                          />
+                          <div className="pointer-events-none absolute top-2/4 left-2/4 -translate-y-2/4 -translate-x-2/4 text-white opacity-0 transition-opacity peer-checked:opacity-100">
+                            <svg
+                              xmlns="http://www.w3.org/2000/svg"
+                              className="h-3.5 w-3.5"
+                              viewBox="0 0 20 20"
+                              fill="currentColor"
+                              stroke="currentColor"
+                              strokeWidth="1"
+                            >
+                              <path
+                                fillRule="evenodd"
+                                d="M16.707 5.293a1 1 0 010 1.414l-8 8a1 1 0 01-1.414 0l-4-4a1 1 0 011.414-1.414L8 12.586l7.293-7.293a1 1 0 011.414 0z"
+                                clipRule="evenodd"
+                              ></path>
+                            </svg>
+                          </div>
+                        </label>
+                        <label className="mt-px cursor-pointer select-none  ">
+                          I have received the payment
+                        </label>
                       </div>
+                      {/* <div className="flex gap-1">
+                        <input
+                          type="checkbox"
+                          className="appearance-none bg-transparent border border-white w-4 h-4 rounded checked:bg-transparent checked:border-white checked:text-green-500"
+                        />
+                        <p>I have received the payment</p>
+                      </div> */}
                       <div>
                         <p>Payment Mode</p>
                         <select>
