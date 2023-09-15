ecc5490df367e1419d4b158b2c66f0867518b290
mohdyaserkt <yaserkt786@gmail.com>
2023-09-15 08:25:16 +0000

    15 Sept 2023, 13:55

---
+++ src/utils/Invoice/getCustomers.ts
@@ -0,0 +1,17 @@
+import { getCustomersforinvoice } from "@/app/api/invoice/getCustomers";
+import { AxiosError } from "axios";
+import "react-toastify/dist/ReactToastify.css";
+
+
+export const handleGetCustomersforinvoice = async (): Promise<
+  string | boolean | AxiosError
+> => {
+  try {
+    const customers = await getCustomersforinvoice();
+    return Promise.resolve(customers as any);
+  } catch (error) {
+    const err = error as AxiosError;
+    console.log(err.message, "errors");
+    return Promise.reject(err);
+  }
+};
