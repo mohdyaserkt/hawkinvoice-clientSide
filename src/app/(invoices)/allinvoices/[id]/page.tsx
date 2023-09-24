9c14451b0e44566281d6abe78634bcc714df2c2c
mohdyaserkt <yaserkt786@gmail.com>
2023-09-24 10:35:53 +0000

    24 Sept 2023, 16:05

--- src/app/(invoices)/allinvoices/[id]/page.tsx
+++ src/app/(invoices)/allinvoices/[id]/page.tsx
@@ -220,7 +220,9 @@ const GetStarted = () => {
                         <div className="flex justify-between w-full ">
                           <div className="  flex flex-col justify-center text-white">
                             <div className="flex flex-col ">
-                            <Link href={`/allinvoices/${item.id}`}><h3 className="text-sm">{item.customerName}</h3></Link>
+                              <Link href={`/allinvoices/${item.id}`}>
+                                <h3 className="text-sm">{item.customerName}</h3>
+                              </Link>
                               <div className="flex gap-2 text-[13px] items-center">
                                 <p className="">{item.invoiceNumber}</p>
                                 <p>|</p>
@@ -251,14 +253,12 @@ const GetStarted = () => {
               </div>
               <div ref={printableRef} className="w-full">
                 <div className="max-w-[85rem] px-4 sm:px-6 lg:px-8 mx-auto my-4 sm:my-10">
-                  {/* Grid */}
-                  <div className="mb-5 pb-5 flex justify-between items-center border-b border-gray-200 dark:border-gray-700">
+                  {/*<div className="mb-5 pb-5 flex justify-between items-center border-b border-gray-200 dark:border-gray-700">
                     <div>
                       <h2 className="text-2xl font-semibold text-gray-800 dark:text-gray-200">
                         Invoice
                       </h2>
                     </div>
-                    {/* Col */}
                     <div className="inline-flex gap-x-2">
                       <a
                         className="py-2 px-3 inline-flex justify-center items-center gap-2 rounded-md border font-medium bg-white text-gray-700 shadow-sm align-middle hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-white focus:ring-blue-600 transition-all text-sm dark:bg-slate-900 dark:hover:bg-slate-800 dark:border-gray-700 dark:text-gray-400 dark:hover:text-white dark:focus:ring-offset-gray-800"
@@ -296,11 +296,8 @@ const GetStarted = () => {
                         Print
                       </a>
                     </div>
-                    {/* Col */}
                   </div>
-                  {/* End Grid */}
 
-                  {/* Grid */}
                   <div className="grid md:grid-cols-2 gap-3">
                     <div>
                       <div className="grid space-y-3">
@@ -357,8 +354,6 @@ const GetStarted = () => {
                         </dl>
                       </div>
                     </div>
-                    {/* Col */}
-
                     <div>
                       <div className="grid space-y-3">
                         <dl className="grid sm:flex gap-x-3 text-sm">
@@ -402,11 +397,8 @@ const GetStarted = () => {
                         </dl>
                       </div>
                     </div>
-                    {/* Col */}
                   </div>
-                  {/* End Grid */}
 
-                  {/* Table */}
                   <div className="mt-6 border border-gray-200 p-4 rounded-lg space-y-4 dark:border-gray-700">
                     <div className="hidden sm:grid sm:grid-cols-5">
                       <div className="sm:col-span-2 text-xs font-medium text-gray-500 uppercase">
@@ -468,24 +460,21 @@ const GetStarted = () => {
                       </>
                     ))}
                   </div>
-                  {/* End Table */}
 
-                  {/* Flex */}
                   <div className="mt-8 flex sm:justify-end">
                     <div className="w-full max-w-2xl sm:text-right space-y-2">
-                      {/* Grid */}
                       <div className="grid grid-cols-2 sm:grid-cols-1 gap-3 sm:gap-2">
                         <dl className="grid sm:grid-cols-5 gap-x-3 text-sm">
                           <dt className="col-span-3 text-gray-500">Subotal:</dt>
                           <dd className="col-span-2 font-medium text-gray-800 dark:text-gray-200">
-                          ₹{mainInvoice?.subTotal}
+                            ₹{mainInvoice?.subTotal}
                           </dd>
                         </dl>
 
                         <dl className="grid sm:grid-cols-5 gap-x-3 text-sm">
                           <dt className="col-span-3 text-gray-500">Total:</dt>
                           <dd className="col-span-2 font-medium text-gray-800 dark:text-gray-200">
-                          ₹{mainInvoice?.Total}
+                            ₹{mainInvoice?.Total}
                           </dd>
                         </dl>
 
@@ -494,7 +483,8 @@ const GetStarted = () => {
                             Amount paid:
                           </dt>
                           <dd className="col-span-2 font-medium text-gray-800 dark:text-gray-200">
-                          ₹{mainInvoice?.status == "paid"
+                            ₹
+                            {mainInvoice?.status == "paid"
                               ? mainInvoice?.Total
                               : 0.0}
                           </dd>
@@ -505,16 +495,16 @@ const GetStarted = () => {
                             Due balance:
                           </dt>
                           <dd className="col-span-2 font-medium text-gray-800 dark:text-gray-200">
-                          ₹{mainInvoice?.status == "paid"
+                            ₹
+                            {mainInvoice?.status == "paid"
                               ? 0
-                              : mainInvoice?.Total}.00
+                              : mainInvoice?.Total}
+                            .00
                           </dd>
                         </dl>
                       </div>
-                      {/* End Grid */}
                     </div>
-                  </div>
-                  {/* End Flex */}
+                  </div> */}
                 </div>
               </div>
             </div>
