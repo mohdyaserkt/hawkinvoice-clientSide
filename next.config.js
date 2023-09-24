2c8604db4a7dd1b0c09b99f368bdb85a55c32350
mohdyaserkt <yaserkt786@gmail.com>
2023-09-24 12:32:37 +0000

    24 Sept 2023, 18:02

--- next.config.js
+++ next.config.js
@@ -1,4 +1,9 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
-
-module.exports = nextConfig
+const nextConfig = {
+    images: {
+      domains: ['img.freepik.com'], // Add the hostname(s) you want to allow
+    },
+  };
+  
+  module.exports = nextConfig;
+  
--- src/app/(invoices)/allinvoices/[id]/page.tsx
+++ src/app/(invoices)/allinvoices/[id]/page.tsx
@@ -253,7 +253,7 @@ const GetStarted = () => {
                 </div>
               </div>
               <div ref={printableRef} className="w-full">
-                <div className="max-w-[85rem] px-4 sm:px-6 lg:px-8 mx-auto my-4 sm:my-10">
+                <div className="max-w-[85rem] px-4 sm:px-6 lg:px-8 mx-auto my-4 sm:my-10 bg-white">
                   {/*<div className="mb-5 pb-5 flex justify-between items-center border-b border-gray-200 dark:border-gray-700">
                     <div>
                       <h2 className="text-2xl font-semibold text-gray-800 dark:text-gray-200">
