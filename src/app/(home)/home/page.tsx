f30cd8cb3140ea81fbb59c35ba263446b7f0250f
mohdyaserkt <yaserkt786@gmail.com>
2023-09-28 08:00:50 +0000

    28 Sept 2023, 13:30

--- src/app/(home)/home/page.tsx
+++ src/app/(home)/home/page.tsx
@@ -129,7 +129,28 @@ const getStarted = () => {
                 <div className="w-full h-[49px] bg-orange-300rounded-lg border rounded-t-lg bg-secondary"></div>
                 <div className="w-full h-[67px] bg-orange-500  border-b border-x"></div>
 
-                <div className="w-full h-[92px] bg-red-900 rounded-b-lg border-b border-x"></div>
+                <div className="w-full h-[92px] bg-red-900 rounded-b-lg border-b border-x flex justify-between">
+                  <div>
+                    <p className="text-xs text-white">current</p>
+                    <h3 className="text-white text-xl">₹0.00</h3>
+                  </div>
+                  <div>
+                    <p className="text-xs text-white">OVERDUE</p>
+                    <h3 className="text-white text-xl">₹0.00</h3>
+                    <p className="text-secondary text-xs">1-15 Days</p>
+                  </div>
+                  <div>
+                    <h3 className="text-white text-xl">₹0.00</h3>
+                    <p className="text-secondary text-xs">16-30 Days</p>
+                  </div>
+                  <div>
+                    <h3 className="text-white text-xl">₹0.00</h3>
+                    <p className="text-secondary text-xs">Above 45 days</p>
+                  </div>
+                  <div>
+                  <h3 className="text-white text-xl">₹0.00</h3>
+                  </div>
+                </div>
               </div>
             </div>
           </div>
