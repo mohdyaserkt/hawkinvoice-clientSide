c243f36ab7f267afa227b075c43bb8c2eb2a3dcf
mohdyaserkt <yaserkt786@gmail.com>
2023-09-29 08:50:02 +0000

    29 Sept 2023, 14:20

--- src/components/home/StackedProgressBar.tsx
+++ src/components/home/StackedProgressBar.tsx
@@ -13,11 +13,12 @@ const StackedProgressBar: React.FC<StackedProgressBarProps> = ({ progressData })
   const total = progressData.reduce((acc, item) => acc + item.value, 0);
 
   return (
-    <div className="bg-gray-300 h-4 w-full rounded-full">
+    <div className="bg-gray-300 h-4 w-full rounded-full relative">
       {progressData.map((item, index) => (
         <div
           key={index}
-          className={`bg-${item.color}-500 h-full w-${(item.value / total) * 100}% rounded-full`}
+          className={`bg-${item.color}-${Math.round((item.value / total) * 100)} h-full absolute left-0 top-0`}
+          style={{ width: `${(item.value / total) * 100}%` }}
         ></div>
       ))}
     </div>
