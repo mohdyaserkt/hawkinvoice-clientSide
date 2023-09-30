cb5e4ef8f4aed38dac3eff340d1d22c74b2b5463
mohdyaserkt <yaserkt786@gmail.com>
2023-09-30 07:05:28 +0000

    30 Sept 2023, 12:35

--- src/app/(home)/home/page.tsx
+++ src/app/(home)/home/page.tsx
@@ -20,6 +20,51 @@ import { ApiError } from "next/dist/server/api-utils";
 import { handleCreateNewItem } from "@/utils/items/createNewItem";
 import Image from "next/image";
 import Example from "@/components/home/chart";
+
+const data = [
+  {
+    name: "Page A",
+    uv: 4000,
+    pv: 2400,
+    amt: 2400
+  },
+  {
+    name: "Page B",
+    uv: 3000,
+    pv: 1398,
+    amt: 2210
+  },
+  {
+    name: "Page C",
+    uv: 2000,
+    pv: 9800,
+    amt: 2290
+  },
+  {
+    name: "Page D",
+    uv: 2780,
+    pv: 3908,
+    amt: 2000
+  },
+  {
+    name: "Page E",
+    uv: 1890,
+    pv: 4800,
+    amt: 2181
+  },
+  {
+    name: "Page F",
+    uv: 2390,
+    pv: 3800,
+    amt: 2500
+  },
+  {
+    name: "Page G",
+    uv: 3490,
+    pv: 4300,
+    amt: 2100
+  }
+];
         
         
 
@@ -191,7 +236,7 @@ const getStarted = () => {
               <div className="w-full h-[49px] bg-orange-300rounded-lg border rounded-t-lg bg-secondary py"></div>
               <div>
                 <div>
-                <Example/>
+                
 
                 </div>
                 <div>
