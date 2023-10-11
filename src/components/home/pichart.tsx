0c38d51c82b760e1df3aa07e5aa5d30f831bb9f4
mohdyaserkt <yaserkt786@gmail.com>
2023-10-11 06:00:34 +0000

    11 Oct 2023, 11:30

--- src/components/home/pichart.tsx
+++ src/components/home/pichart.tsx
@@ -1,36 +1,53 @@
-import React from "react";
+import { handleGetExpenseChartData } from "@/utils/home/getExpenseChartData";
+import React, { useEffect, useState } from "react";
 import { Chart } from "react-google-charts";
 
-const data = [
-  ["Expense Type", "Percentage"],
-  ["Labor", 40.44],
-  ["Travel Expense", 20.12],
-  ["Rent Expense", 20.12],
-  ["Materials", 19.32],
-];
+const ExpensePieChart = () => {
+  const [data, setData] = useState([
+    ["Expense Type", "Percentage"],
+    ["Labor", 40.44],
+    ["Travel Expense", 20.12],
+    ["Rent Expense", 20.12],
+    ["Materials", 19.32],
+  ]);
 
-const options = {
-  // title: "Expense Breakdown",
-  backgroundColor: "#143230",
-  legend: {
-    textStyle: {
-      color: "white",
+  const [expenseData, setexpenseData] = useState<any[]>([]);
+  const options = {
+    // title: "Expense Breakdown",
+    backgroundColor: "#143230",
+    legend: {
+      textStyle: {
+        color: "white",
+      },
     },
-  },
-  chartArea: {
-    // leave room for y-axis labels
-    width: '100%'
-  },
-};
+    chartArea: {
+      // leave room for y-axis labels
+      width: "100%",
+    },
+  };
 
-const ExpensePieChart = () => (
-  <Chart
-    chartType="PieChart"
-    data={data}
-    options={options}
-    width={"100%"}
-    height={"375px"}
-  />
-);
+  useEffect(() => {
+    handleGetExpenseChartData()
+      .then(({ data }: any) => {
+        setexpenseData(data.expenseChartData);
+      })
+      .catch((err) => {
+        console.log(err);
+        alert(err);
+      });
+  }, []);
+
+  return (
+    <div>
+      <Chart
+        chartType="PieChart"
+        data={data}
+        options={options}
+        width={"100%"}
+        height={"375px"}
+      />
+    </div>
+  );
+};
 
 export default ExpensePieChart;
