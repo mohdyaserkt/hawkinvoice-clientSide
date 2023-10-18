39227acfd452da0cbabd0a207d0d7086aaa4d325
mohdyaserkt <yaserkt786@gmail.com>
2023-10-18 09:19:01 +0000

    18 Oct 2023, 14:49

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,22 +1,29 @@
-import { useAppSelector } from '@/redux/store';
-import Image from 'next/image'
-import { FcGoogle } from 'react-icons/fc';
-import { useSelector } from 'react-redux';
+"use client";
+import { logIn } from "@/redux/features/auth-slice";
+import { AppDispatch, useAppSelector } from "@/redux/store";
+import Image from "next/image";
+import { FcGoogle } from "react-icons/fc";
+import { useDispatch } from "react-redux";
 
 export default function Home() {
+  const dispatch = useDispatch<AppDispatch>();
 
-  let data= useAppSelector((state)=> state.authReducer.value)
-  console.log(data,"my data   knkjk");
-  
+  let data = useAppSelector((state) => state.authReducer.value);
+
+  let email = "check@gmail.com";
+
+  console.log(data, "my data   knkjk");
   return (
     <>
-      <div className='w-screen h-screen '>
+      <div className="w-screen h-screen ">
         <nav></nav>
-        <div className='flex h-full justify-center items-center text-secondary text-6xl '>
-        HOME PAGE
-
+        <div
+          onClick={() => dispatch(logIn(email))}
+          className="flex h-full justify-center items-center text-secondary text-6xl "
+        >
+          HOME PAGE
         </div>
       </div>
     </>
-  )
+  );
 }
