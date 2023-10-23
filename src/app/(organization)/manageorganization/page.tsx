e6b4160b689d09eeaa7c38df5b6ba4a5a737c049
mohdyaserkt <yaserkt786@gmail.com>
2023-10-23 06:34:26 +0000

    23 Oct 2023, 12:04

--- src/app/(organization)/manageorganization/page.tsx
+++ src/app/(organization)/manageorganization/page.tsx
@@ -125,36 +125,36 @@ const manageOrganisation = () => {
               </div>
             ))}
             <div>
-            <div>
-              <div className="flex w-full border-[1px] border-white rounded-lg p-6">
-                <div className="w-2/12 h-32 mr-6 bg-white rounded flex justify-center items-center">
-                  <svg
-                    xmlns="http://www.w3.org/2000/svg"
-                    fill="none"
-                    viewBox="0 0 24 24"
-                    strokeWidth="1.5"
-                    stroke="currentColor"
-                    className="w-3/5 h-3/5 text-slate-600"
-                  >
-                    <path
-                      stroke-linecap="round"
-                      stroke-linejoin="round"
-                      d="M2.25 21h19.5m-18-18v18m10.5-18v18m6-13.5V21M6.75 6.75h.75m-.75 3h.75m-.75 3h.75m3-6h.75m-.75 3h.75m-.75 3h.75M6.75 21v-3.375c0-.621.504-1.125 1.125-1.125h2.25c.621 0 1.125.504 1.125 1.125V21M3 3h12m-.75 4.5H21m-3.75 3.75h.008v.008h-.008v-.008zm0 3h.008v.008h-.008v-.008zm0 3h.008v.008h-.008v-.008z"
-                    />
-                  </svg>
-                </div>
-                <div className="w-full flex items-center justify-center">
-                  
-                    <h1 className="text-base text-blue-400 text-center">Currently, you don't have any organization established, so it's essential to create one to further movements!!!</h1>
-                   
-                  
-                 
+              <div>
+                <div className="flex w-full border-[1px] border-white border-dashed rounded-lg p-6">
+                  <div className="w-2/12 h-32 mr-6 bg-white rounded flex justify-center items-center">
+                    <svg
+                      xmlns="http://www.w3.org/2000/svg"
+                      fill="none"
+                      viewBox="0 0 24 24"
+                      strokeWidth="1.5"
+                      stroke="currentColor"
+                      className="w-3/5 h-3/5 text-slate-600"
+                    >
+                      <path
+                        stroke-linecap="round"
+                        stroke-linejoin="round"
+                        d="M2.25 21h19.5m-18-18v18m10.5-18v18m6-13.5V21M6.75 6.75h.75m-.75 3h.75m-.75 3h.75m3-6h.75m-.75 3h.75m-.75 3h.75M6.75 21v-3.375c0-.621.504-1.125 1.125-1.125h2.25c.621 0 1.125.504 1.125 1.125V21M3 3h12m-.75 4.5H21m-3.75 3.75h.008v.008h-.008v-.008zm0 3h.008v.008h-.008v-.008zm0 3h.008v.008h-.008v-.008z"
+                      />
+                    </svg>
+                  </div>
+                  <div className="w-full flex items-center justify-center">
+                    <h1 className="text-base text-blue-500 text-center">
+                      {" "}
+                      &#9888; Currently, you don't have any organization
+                      established, so it's essential to create one to further
+                      movements!!!&#9888;
+                    </h1>
+                  </div>
                 </div>
               </div>
             </div>
           </div>
-          </div>
-          
         </div>
       </div>
     </div>
