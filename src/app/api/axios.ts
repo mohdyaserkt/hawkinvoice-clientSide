e1e85622887ffe7b7f2013773a7e0c7f42acac8d
mohdyaserkt <yaserkt786@gmail.com>
2023-10-23 05:39:39 +0000

    23 Oct 2023, 11:09

--- src/app/api/axios.ts
+++ src/app/api/axios.ts
@@ -1,27 +1,35 @@
-import { useAppSelector } from "@/redux/store";
+
+import { persistStore } from 'redux-persist';
+
 import axios, { AxiosError, AxiosInstance } from "axios";
+import { store } from '@/redux/store';
+const persistor = persistStore(store);
+const state = store.getState();
+
+
+
 const axiosInstance: AxiosInstance = axios.create({
   baseURL: "https://server.hawkinvoice.online",
 });
 
 axiosInstance.interceptors.request.use(
   (config) => {
-    const User = useAppSelector((state) => state.authReducer.value.AccessToken);
-    if (User) {
-      const AccessToken = User;
-      if (AccessToken) {
-        config.headers["Authorization"] = `Bearer ${AccessToken}`;
-      }
+    // const User = useAppSelector((state) => state.authReducer.value.AccessToken);
+    // if (User) {
+    //   const AccessToken = User;
+    //   if (AccessToken) {
+    //     config.headers["Authorization"] = `Bearer ${AccessToken}`;
+    //   }
 
-      const currentOrganization = useAppSelector(
-        (state) => state.orgReducer.value.id
-      );
-      console.log("this is my current company id ", currentOrganization);
+    //   const currentOrganization = useAppSelector(
+    //     (state) => state.orgReducer.value.id
+    //   );
+    //   console.log("this is my current company id ", currentOrganization);
 
-      if (currentOrganization) {
-        config.headers["currentOrganization"] = currentOrganization;
-      }
-    }
+    //   if (currentOrganization) {
+    //     config.headers["currentOrganization"] = currentOrganization;
+    //   }
+    // }
 
     return config;
   },
